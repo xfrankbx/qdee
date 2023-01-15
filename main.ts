d69aec7c4f6f006559329206fcafba4cdc70f0b9
Frank B <1663680+xfrankbx@users.noreply.github.com>
2023-01-15 18:42:33 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
 let USDistance = 0
 basic.showIcon(IconNames.SmallDiamond)
-basic.pause(1000)
+basic.pause(100)
 basic.showIcon(IconNames.Diamond)
-basic.pause(1000)
+basic.pause(100)
 basic.showIcon(IconNames.Yes)
-basic.pause(1000)
+basic.pause(200)
 qdee.qdee_Init()
+basic.clearScreen()
 basic.forever(function () {
-    basic.clearScreen()
-    while (true) {
-        USDistance = qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2)
-        basic.showString("" + (USDistance))
-        basic.pause(1000)
+    USDistance = qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2)
+    if (USDistance <= 15) {
+        basic.showIcon(IconNames.Sad)
+        qdee.qdee_setMotorSpeed(-50, 50)
+    } else {
+        basic.showIcon(IconNames.Happy)
+        qdee.qdee_setMotorSpeed(50, 50)
     }
 })
